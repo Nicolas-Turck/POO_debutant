2cd8d810d72a6af72680873502a4a19e6c4cba38
nicos <nicolas.turck@gmail.com>
2019-12-01 16:30:21 +0100

    add exercice 3 in folder ex3 and create readme.md

---
+++ ex3/books_list.py
@@ -0,0 +1,15 @@
+# coding: utf8
+books = []
+title = []
+books_list = [
+    ['Katinka', ['page1', 'page2', 'page3', 'page4', 'page5']],
+    ['La légende de Gösta Berling', ['page1', 'page2', 'page3', 'page4', 'page5']],
+    ['Le père Goriot', ['page1', 'page2', 'page3', 'page4', 'page5']],
+    ['Harry Potter', ['page1', 'page2', 'page3', 'page4', 'page5']],
+    ['La nuit des temps', ['page1', 'page2', 'page3', 'page4', 'page5']],
+    ['Les pensées', ['page1', 'page2', 'page3', 'page4', 'page5']]
+]
+
+
+
+
